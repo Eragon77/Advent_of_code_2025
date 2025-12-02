a4d82d8073fa81e23291d7a4b05246e5755a4731
Luca <lquare04@gmail.com>
2025-12-02 10:15:31 +0100

    Day 1

---
+++ day1/dial=[].py
@@ -0,0 +1,38 @@
+dial=[]
+for i in range (100):
+    dial.append(i)
+
+f=open("input.txt")
+input=f.readline()
+i=50
+total=0
+
+while input!="":
+    if input[0]=="R":
+        rot=int(input[1:])
+
+        for j in range(rot):
+            if(i==99):
+                i=0
+            else:
+                i+=1
+
+    elif input[0]=="L":
+        rot=int(input[1:])
+
+        for j in range(rot):
+            if i==0:
+                i=99
+            else:
+                i-=1
+    if(i==0):
+        total+=1
+    input=f.readline()
+
+print(total)
+
+
+
+
+
+
